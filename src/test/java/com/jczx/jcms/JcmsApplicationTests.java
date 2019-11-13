bc9d68f959acb6cb2d0e157b5b6a274198979cda
swj <1440474949@qq.com>
2019-11-13 11:08:45 +0800

    根据自己模块建立相依的页面,准守命名规则

---
+++ src/test/java/com/jczx/jcms/JcmsApplicationTests.java
@@ -0,0 +1,20 @@
+package com.jczx.jcms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+    /*@Test
+    public void initContext() {
+        System.out.println("-------------test  start-------------");
+        configScanner.scan();
+        domainUtil.generateAllSingleTable();
+
+
+    }*/
+}
