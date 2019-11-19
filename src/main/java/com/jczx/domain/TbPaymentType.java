76ab12184c0cecedd88d45ba49fe903a542444ce
37346 <373464380@qq.com>
2019-11-19 14:26:17 +0800

    111

---
+++ src/main/java/com/jczx/domain/TbPaymentType.java
@@ -0,0 +1,48 @@
+package com.jczx.domain;
+
+import net.atomarrow.domains.Domain;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author 丛枭钰
+ * @create 2019-11-19 14:15
+ */
+@Component
+public class TbPaymentType extends Domain {
+    private Integer id;
+    private String name;
+    private Integer operatorId;//操作人id
+    private String createTime;//操作时间
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getOperatorId() {
+        return operatorId;
+    }
+
+    public void setOperatorId(Integer operatorId) {
+        this.operatorId = operatorId;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+}
