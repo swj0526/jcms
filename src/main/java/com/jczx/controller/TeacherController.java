184251654f5d0f897f09986a0ecc046aa4b173ed
Song <1532544432@qq.com>
2019-11-26 22:40:51 +0800

    sjx

--- src/main/java/com/jczx/controller/TeacherController.java
+++ src/main/java/com/jczx/controller/TeacherController.java
@@ -4,11 +4,16 @@ import com.jczx.domain.TbTeacher;
 import com.jczx.service.TeacherService;
 import net.atomarrow.bean.Pager;
 import net.atomarrow.bean.ServiceResult;
+import net.atomarrow.render.Render;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.InputStream;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -30,21 +35,34 @@ public class TeacherController extends BaseController{
     @RequestMapping("/add")
     @ResponseBody
     public ServiceResult addTeacher(TbTeacher teacher) {
+        Date date = new Date();
+        SimpleDateFormat dateformat = new SimpleDateFormat("YYYY-MM-dd");
+        String time = dateformat.format(date);
+        Date createTime = null;
+        try {
+            createTime = dateformat.parse(time);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        teacher.setCreateTime(createTime);
         ServiceResult add = teacherService.addTeacher(teacher);
-        System.out.println("页面"+add.isSuccess());
         return add;
     }
 
     @RequestMapping("/modify")
     @ResponseBody
     public ServiceResult modifyTeacher(TbTeacher teacher){
+        System.out.println(teacher.getName());
+        System.out.println(teacher.getPhone());
+        System.out.println(teacher.getHasQuit());
+
         ServiceResult modify=teacherService.modifyTeacher(teacher);
         return modify;
     }
 
     @RequestMapping("/list")
     @ResponseBody
-    public ServiceResult teacherList(String name, boolean hasQuit, int page, int limit){
+    public ServiceResult teacherList(String name,boolean hasQuit, int page, int limit){
         Pager pager = checkPager(limit, page);
         List<TbTeacher> list= teacherService.teacherList(name,hasQuit,pager);
         System.out.println(name);
@@ -53,16 +71,20 @@ public class TeacherController extends BaseController{
     }
 
     @RequestMapping("/delete")
+    @ResponseBody
     public ServiceResult deleteTeacher(TbTeacher teacher){
         ServiceResult delete= teacherService.deleteTeacher(teacher);
         return delete;
     }
 
-    /*@RequestMapping("/doExcel")
+    @RequestMapping("/doExcel")
     @ResponseBody
-    public Render doExcel(TbTeacher teacher,PageBean pageBean){
-        InputStream inputStream = teacherService.teacherExcel(teacher,pageBean);
+    public Render doExcel(String name,boolean hasQuit){
+        Pager pager = new Pager();
+        pager.setPageSize(100000);
+        pager.setCurrentPage(1);
+        InputStream inputStream = teacherService.teacherExcel(name,hasQuit,pager);
         return Render.renderFile("老师信息.xls", inputStream);
-    }*/
+    }
 
 }
--- src/main/java/com/jczx/service/TeacherService.java
+++ src/main/java/com/jczx/service/TeacherService.java
@@ -5,8 +5,12 @@ import net.atomarrow.bean.Pager;
 import net.atomarrow.bean.ServiceResult;
 import net.atomarrow.db.parser.Conditions;
 import net.atomarrow.services.Service;
+import net.atomarrow.util.StringUtil;
+import net.atomarrow.util.excel.ExcelDatas;
+import net.atomarrow.util.excel.ExcelUtil;
 import org.springframework.stereotype.Component;
 
+import java.io.InputStream;
 import java.util.List;
 
 /**
@@ -16,23 +20,41 @@ import java.util.List;
  */
 @Component
 public class TeacherService  extends Service {
+    Conditions conditions = new Conditions(TbTeacher.class);
     /**
      * 添加老师
-     * @param teacher
+     * @param tbTeacher
      * @return
      */
-    public ServiceResult addTeacher(TbTeacher teacher) {
-        add(teacher);
+    public ServiceResult addTeacher(TbTeacher tbTeacher) {
+        conditions.putEW("phone",tbTeacher.getPhone());
+        List<TbTeacher> list = getList(conditions);
+        if (list.size()!=0){
+            return error("手机号重复");
+        }
+        if(StringUtil.isBlank(tbTeacher.getName())||StringUtil.isBlank(tbTeacher.getPhone())){
+            return error("请输入姓名和手机号");
+        }
+
+        add(tbTeacher);
         return SUCCESS;
     }
 
     /**
      * 修改老师信息
-     * @param teacher
+     * @param tbTeacher
      * @return
      */
-    public ServiceResult modifyTeacher(TbTeacher teacher){
-        modify(teacher);
+    public ServiceResult modifyTeacher(TbTeacher tbTeacher){
+        conditions.putEW("phone",tbTeacher.getPhone());
+        List<TbTeacher> list = getList(conditions);
+        if (list.size()!=0){
+            return error("手机号重复");
+        }
+        if(StringUtil.isBlank(tbTeacher.getName())||StringUtil.isBlank(tbTeacher.getPhone())){
+            return error("请输入姓名和手机号");
+        }
+        modify(tbTeacher);
         return SUCCESS;
     }
 
@@ -41,7 +63,6 @@ public class TeacherService  extends Service {
      * @return
      */
     public List<TbTeacher> teacherList(String name,boolean hasQuit,Pager pager){
-        Conditions conditions = new Conditions(TbTeacher.class);
         conditions.putEWIfOk("name",name);
         conditions.putEWIfOk("hasQuit",hasQuit);
         pager.setDataTotal(getCount(conditions));//调用分页之前给设置总条数
@@ -60,19 +81,20 @@ public class TeacherService  extends Service {
     }
 
     /**
-     * 导出excel
-     * @param bean
-     * @param pageBean
+     * 导出
+     * @param name
+     * @param hasQuit
+     * @param pager
      * @return
      */
-    /*public InputStream teacherExcel(TbTeacher bean, PageBean pageBean) {
+    public InputStream teacherExcel(String name,boolean hasQuit, Pager pager) {
         ExcelDatas excelDatas = new ExcelDatas();
-        List<TbTeacher> list = teacherList(bean,pageBean);
+        List<TbTeacher> list = teacherList(name,hasQuit,pager);
         excelDatas.addStringArray(0, 0, new String[]{"id","姓名","性别", "手机号", "是否在职"});
         excelDatas.addObjectList(1, 0, list, new String[]{"id","name", "gender", "phone", "hasQuit"});//行,列,集合
         InputStream inputStream = ExcelUtil.exportExcel(excelDatas);
         return inputStream;
 
-    }*/
+    }
 
 }
